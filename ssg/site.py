779ead51c70bebd3fbd0d49be8d4ecb9f3b3b55d
Mooszek <Mooszek@gmail.com>
2021-11-29 22:25:57 +0100

    Parser class content

--- ssg/site.py
+++ ssg/site.py
@@ -1,14 +1,16 @@
 from genericpath import isdir
 from os import mkdir
 from pathlib import Path
+from parsers import Parser
 
 
 class Site:
 
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.self = self
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -21,3 +23,17 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir(path):
                 self.create_dir(path)
+            elif path.is_file(path):
+                self.run_parser(path)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if extension == parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not implemented")
